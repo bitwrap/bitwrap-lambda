31337c8b3cfded234cce3db0eaea3d6b831e2d3e
stackdump <myork@stackdump.com>
2017-03-15 17:10:14 -0700

    WIP - adding keen.io

--- dist/main.py
+++ dist/main.py
@@ -5,12 +5,13 @@ https://github.com/bitwrap/bitwrap-io/blob/master/bitwrap_io/_lambda.py
 """
 
 from bitwrap_io import _lambda
+import keen
 
 def handler(event, context):
 
     res = _lambda.handler(event, context)
 
     if event['path'] == '/api' and 'body' in res:
-        print res['body']
+        keen.post(res['body'], schema=event['pathParameters']['schema'])
 
     return res
