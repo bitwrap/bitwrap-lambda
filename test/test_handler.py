1291445106e591d9560400e86d0439e86d744ac6
stackdump <myork@stackdump.com>
2017-07-03 13:41:35 -0500

    test should init db

--- test/test_handler.py
+++ test/test_handler.py
@@ -3,6 +3,8 @@ import os
 import sys
 import json
 import unittest
+import bitwrap_psql.db as pg
+import bitwrap_machine as pnml
 
 sys.path.append(os.path.abspath(__file__ + '/../'))
 import mocks
@@ -11,6 +13,12 @@ import main as Api
 
 class LamdaTest(unittest.TestCase):
     """ test api methods not dealing with events"""
+
+    def setUp(self):
+        pg.create_db(pnml.Machine('counter'), drop=True, **Api.options)
+        self.store = Api.eventstore('counter')
+        self.store.db.create_stream('foo')
+
 
     def test_lambda_handler(self):
         """ test a sequence of tic-tac-toe transformations """
