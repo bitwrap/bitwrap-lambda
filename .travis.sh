123e55bdf2278db6e342e9e92f501e58ea6a7524
stackdump <myork@stackdump.com>
2017-07-08 23:16:19 -0500

    WIP

--- .travis.sh
+++ .travis.sh
@@ -12,4 +12,4 @@ pip install -e git+https://github.com/bitwrap/bitwrap-psql.git#egg=bitwrap-psql
 cp -r $VIRTUAL_ENV/src/bitwrap-machine/bitwrap_machine  dist/
 cp -r $VIRTUAL_ENV/src/bitwrap-psql/bitwrap_psql  dist/
 
-pip install -t $PWD/dist -r requirements.txt
+pip install -t $PWD/dist pg8000==1.10.6
